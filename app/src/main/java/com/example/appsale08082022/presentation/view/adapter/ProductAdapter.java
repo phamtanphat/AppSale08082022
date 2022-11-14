0a21f9871b79aec80bb7d4060c53c2a58e7a32c2
Phat <phatdroid94@gmail.com>
2022-11-14 21:27:19 +0700

    adapter

---
+++ app/src/main/java/com/example/appsale08082022/presentation/view/adapter/ProductAdapter.java
@@ -0,0 +1,13 @@
+package com.example.appsale08082022.presentation.view.adapter;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * Created by pphat on 11/14/2022.
+ */
+public class ProductAdapter {
+
+    class ProductViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
